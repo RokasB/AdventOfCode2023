c1023d123317dc0fda414acb60db7b9dd52b21c6
dev <dev@DESKTOP>
2023-12-18 21:10:09 +0200

    Day 8 part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -17,7 +17,7 @@ namespace Day8
         static void Main(string[] args)
         {
             ReadInput();
-            Console.WriteLine(GetSteps());
+            Console.WriteLine(GetGhostSteps());
             Console.ReadLine();
         }
 
@@ -61,6 +61,80 @@ namespace Day8
                     count++;
                 }
             }
+        }
+
+        public static int GetGhostSteps(string startNode)
+        {
+            int count = 0;
+            string currentNode = startNode;
+
+            while (true)
+            {
+                foreach (var instruction in instructions)
+                {
+                    if (currentNode.EndsWith("Z"))
+                        return count;
+
+                    if (instruction == 'L')
+                    {
+                        currentNode = nodes[currentNode].Left;
+                    }
+                    else//Assuming 'R'
+                    {
+                        currentNode = nodes[currentNode].Right;
+                    }
+                    count++;
+                }
+            }
+        }
+
+        //Get least common multiple of each ghost paths
+        //https://math.libretexts.org/Courses/Mount_Royal_University/MATH_2150%3A_Higher_Arithmetic/4%3A_Greatest_Common_Divisor_least_common_multiple_and_Euclidean_Algorithm/4.3%3A_Least_Common_Multiple
+        public static long GetGhostSteps()
+        {
+            List<string> ghostPaths = new List<string>();
+            ghostPaths = nodes.Where(p => p.Key.EndsWith("A")).Select(p => p.Key).ToList();
+
+            List<int> pathSteps = new List<int>();
+
+            foreach(var ghostPath in ghostPaths)
+            {
+                pathSteps.Add(GetGhostSteps(ghostPath));
+            }
+
+            foreach(var pathStep in pathSteps)
+            {
+                Console.WriteLine(pathStep);
+            }
+            List<int> testList = new List<int>() { 21883, 13019, 11911, 16897 };
+            return LCMOfList(pathSteps);
+        }
+
+        //Purely copypasta from ChGippity
+        static long GCD(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        static long LCM(long a, long b)
+        {
+            return (a / GCD(a, b)) * b;  // Watch out for the order to avoid overflow
+        }
+
+        static long LCMOfList(List<int> numbers)
+        {
+            long result = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                result = LCM(result, numbers[i]);
+            }
+            return result;
         }
     }
 }
